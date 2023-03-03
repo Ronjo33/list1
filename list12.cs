25a0c556cde1cc8f622dd844ba315e6e2b0251c3
User <User@DESKTOP-805DRN8>
2023-03-03 12:59:52 +0200

    Add project files.

---
+++ list12.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumnaOburnati4isla2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            num.Reverse();
+            int sum = 0;
+            for (int i = 0; i < num.Count; i++)
+            {
+                int currNum = num[i];
+                int reversedNum = 0;
+                while (currNum > 0)
+                {
+                    reversedNum = reversedNum * 10 + currNum % 10;
+                    currNum /= 10;
+                }
+                sum += reversedNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
